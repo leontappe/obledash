ab7b16d921af515c8e100addf5675e4219d9d954
Leon Tappe <ltappe@mail.uni-paderborn.de>
2025-05-19 21:11:53 +0200

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,195 @@
-#include <Arduino.h>
-#include "ELMduino.h"
-#include <BLEClientSerial.h>
-#include <esp32_smartdisplay.h>
+/* Using LVGL with Arduino requires some extra steps...
+ *  
+ * Be sure to read the docs here: https://docs.lvgl.io/master/integration/framework/arduino.html
+ * but note you should use the lv_conf.h from the repo as it is pre-edited to work.
+ * 
+ * You can always edit your own lv_conf.h later and exclude the example options once the build environment is working.
+ * 
+ * Note you MUST move the 'examples' and 'demos' folders into the 'src' folder inside the lvgl library folder 
+ * otherwise this will not compile, please see README.md in the repo.
+ * 
+ */
+#include <lvgl.h>
 
-BLEClientSerial BLESerial;
+#include <TFT_eSPI.h>
 
-#define DEBUG_PORT Serial
-#define ELM_PORT BLESerial
+#include <examples/lv_examples.h>
+#include <demos/lv_demos.h>
 
-#define BAUDRATE 115200
+#include <XPT2046_Touchscreen.h>
+// A library for interfacing with the touch screen
+//
+// Can be installed from the library manager (Search for "XPT2046")
+//https://github.com/PaulStoffregen/XPT2046_Touchscreen
+// ----------------------------
+// Touch Screen pins
+// ----------------------------
 
-ELM327 myELM327;
+// The CYD touch uses some non default
+// SPI pins
 
-uint32_t rpm = 0;
+#define XPT2046_IRQ 36
+#define XPT2046_MOSI 32
+#define XPT2046_MISO 39
+#define XPT2046_CLK 25
+#define XPT2046_CS 33
+SPIClass touchscreenSpi = SPIClass(VSPI);
+XPT2046_Touchscreen touchscreen(XPT2046_CS, XPT2046_IRQ);
+uint16_t touchScreenMinimumX = 200, touchScreenMaximumX = 3700, touchScreenMinimumY = 240,touchScreenMaximumY = 3800;
 
-auto lv_last_tick = millis();
+/*Set to your screen resolution*/
+#define TFT_HOR_RES   320
+#define TFT_VER_RES   240
+
+/*LVGL draw into this buffer, 1/10 screen size usually works well. The size is in bytes*/
+#define DRAW_BUF_SIZE (TFT_HOR_RES * TFT_VER_RES / 10 * (LV_COLOR_DEPTH / 8))
+
+#if LV_USE_LOG != 0
+void my_print( lv_log_level_t level, const char * buf )
+{
+    LV_UNUSED(level);
+    Serial.println(buf);
+    Serial.flush();
+}
+#endif
+
+/* LVGL calls it when a rendered image needs to copied to the display*/
+void my_disp_flush( lv_display_t *disp, const lv_area_t *area, uint8_t * px_map)
+{
+    /*Call it to tell LVGL you are ready*/
+    lv_disp_flush_ready(disp);
+}
+/*Read the touchpad*/
+void my_touchpad_read( lv_indev_t * indev, lv_indev_data_t * data )
+{
+  if(touchscreen.touched())
+  {
+    TS_Point p = touchscreen.getPoint();
+    //Some very basic auto calibration so it doesn't go out of range
+    if(p.x < touchScreenMinimumX) touchScreenMinimumX = p.x;
+    if(p.x > touchScreenMaximumX) touchScreenMaximumX = p.x;
+    if(p.y < touchScreenMinimumY) touchScreenMinimumY = p.y;
+    if(p.y > touchScreenMaximumY) touchScreenMaximumY = p.y;
+    //Map this to the pixel position
+    data->point.x = map(p.x,touchScreenMinimumX,touchScreenMaximumX,1,TFT_HOR_RES); /* Touchscreen X calibration */
+    data->point.y = map(p.y,touchScreenMinimumY,touchScreenMaximumY,1,TFT_VER_RES); /* Touchscreen Y calibration */
+    data->state = LV_INDEV_STATE_PRESSED;
+    /*
+    Serial.print("Touch x ");
+    Serial.print(data->point.x);
+    Serial.print(" y ");
+    Serial.println(data->point.y);
+    */
+  }
+  else
+  {
+    data->state = LV_INDEV_STATE_RELEASED;
+  }
+}
+
+lv_indev_t * indev; //Touchscreen input device
+uint8_t* draw_buf;  //draw_buf is allocated on heap otherwise the static area is too big on ESP32 at compile
+uint32_t lastTick = 0;  //Used to track the tick timer
 
 void setup()
 {
-  // init display
-  smartdisplay_init();
+  //Some basic info on the Serial console
+  String LVGL_Arduino = "LVGL demo ";
+  LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
+  Serial.begin(115200);
+  Serial.println(LVGL_Arduino);
+    
+  //Initialise the touchscreen
+  touchscreenSpi.begin(XPT2046_CLK, XPT2046_MISO, XPT2046_MOSI, XPT2046_CS); /* Start second SPI bus for touchscreen */
+  touchscreen.begin(); /* Touchscreen init */
+  touchscreen.setRotation(3); /* Inverted landscape orientation to match screen */
 
-  lv_display_t *display = lv_display_get_default();
-  // lv_display_set_rotation(display, LV_DISPLAY_ROTATION_90);
-  // lv_display_set_rotation(display, LV_DISPLAY_ROTATION_180);
-  // lv_display_set_rotation(display, LV_DISPLAY_ROTATION_270);
+  //Initialise LVGL
+  lv_init();
+  draw_buf = new uint8_t[DRAW_BUF_SIZE];
+  lv_display_t * disp;
+  disp = lv_tft_espi_create(TFT_HOR_RES, TFT_VER_RES, draw_buf, DRAW_BUF_SIZE);
 
-  // init BLE
-  const char *deviceName = "OBDBLE";
-  DEBUG_PORT.begin(BAUDRATE);
-  ELM_PORT.begin((char *)deviceName);
+  //Initialize the XPT2046 input device driver
+  indev = lv_indev_create();
+  lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);  
+  lv_indev_set_read_cb(indev, my_touchpad_read);
 
-  if (!ELM_PORT.connect())
-  {
-    DEBUG_PORT.println("Couldn't connect to OBD scanner - Phase 1");
-    while (1)
-      ;
-  }
+  /* Try an example. See all the examples 
+   * online: https://docs.lvgl.io/master/examples.html
+   */
+  // lv_example_animimg_1();
+  // lv_example_arc_1();
+  // lv_example_arc_2();
+  // lv_example_bar_1();
+  // lv_example_bar_2();
+  // lv_example_bar_3();
+  // lv_example_bar_4();
+  // lv_example_bar_5();
+  // lv_example_bar_6();
+  // lv_example_btn_1();
+  // lv_example_btn_2();
+  // lv_example_btn_3();
+  // lv_example_btnmatrix_1();
+  // lv_example_btnmatrix_2();
+  // lv_example_btnmatrix_3();
+  // lv_example_calendar_1();
+  // lv_example_chart_1();
+  // lv_example_chart_2();
+  // lv_example_chart_3();
+  // lv_example_chart_4();
+  // lv_example_chart_5();
+  // lv_example_chart_6();
+  // lv_example_chart_7();
+  // lv_example_chart_8();
+  // lv_example_chart_9();
+  // lv_example_checkbox_1();
+  // lv_example_checkbox_2();
+  // lv_example_dropdown_1();
+  // lv_example_dropdown_2();
+  // lv_example_dropdown_3();
+  // lv_example_keyboard_1();
+  // lv_example_label_1();
+  // lv_example_label_2();
+  // lv_example_label_3();
+  // lv_example_label_4();
+  // lv_example_label_5();
+  // lv_example_line_1();
+  // lv_example_list_1();
+  // lv_example_list_2();
+  // lv_example_msgbox_1();
+  // lv_example_roller_1();
+  // lv_example_roller_2();
+  // lv_example_roller_3();
+  // lv_example_slider_1();
+  // lv_example_slider_2();
+  // lv_example_slider_3();
+  // lv_example_span_1();
+  // lv_example_spinbox_1();
+  // lv_example_spinner_1();
+  // lv_example_switch_1();
+  // lv_example_table_1();
+  // lv_example_table_2();
+  // lv_example_tabview_1();
+  // lv_example_tabview_2();
+  // lv_example_textarea_1();
+  // lv_example_textarea_2();
+  // lv_example_textarea_3();
+  // lv_example_tileview_1();
+  // lv_example_win_1();
+  
+  //Or try out the large standard widgets demo
+  //// lv_demo_benchmark();          
+  // lv_demo_keypad_encoder();     
 
-  if (!myELM327.begin(ELM_PORT, true, 2000))
-  {
-    Serial.println("Couldn't connect to OBD scanner - Phase 2");
-    while (1)
-      ;
-  }
-
-  Serial.println("Connected to ELM327");
+  //Done
+  Serial.println( "Setup done" );
 }
 
 void loop()
-{
-  auto const now = millis();
-  // Update the ticker
-  lv_tick_inc(now - lv_last_tick);
-  lv_last_tick = now;
-  // Update the UI
-  lv_timer_handler();
-
-  float tempRPM = myELM327.rpm();
-
-  if (myELM327.nb_rx_state == ELM_SUCCESS)
-  {
-    rpm = (uint32_t)tempRPM;
-    Serial.print("RPM: ");
-    Serial.println(rpm);
-  }
-  else if (myELM327.nb_rx_state != ELM_GETTING_MSG)
-  {
-    myELM327.printError();
-  }
+{   
+    lv_tick_inc(millis() - lastTick); //Update the tick timer. Tick is new for LVGL 9
+    lastTick = millis();
+    lv_timer_handler();               //Update the UI
+    delay(5);
 }
